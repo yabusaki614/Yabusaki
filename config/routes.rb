faa8ca910363c82a63cc421fd60c30f7f78b828a
Your Name <you@example.com>
2017-08-08 10:33:45 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #  get '/top' => 'home#top'
+  root 'home#top'
+
+end
