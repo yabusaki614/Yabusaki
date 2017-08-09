c12a790559f1d25bbf407b1579648dfadf43c4b7
Your Name <you@example.com>
2017-08-09 13:08:07 +0000

    kadai

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #  get '/top' => 'home#top'
+  #get '/top' => 'home#top'
   root 'home#top'
-
+  get '/articles' => 'article#index'
 end
