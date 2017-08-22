3079c79c7978bd0659d64449c97744c119597a15
Your Name <you@example.com>
2017-08-22 11:01:56 +0000

    third

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,28 @@
 class ArticlesController < ApplicationController
 	def index
+		#いっぱいデータが入っているよ　の　複数形
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+		#ダミー　新規投稿だと教えている
+	end
+	def create
+		#これから使うものだよ～
+		@article = Article.new(article_params)
+		#@article = Article.create(article_params)でも出来る。その場合.saveはいらない
+    	@article.save
+    	redirect_to '/articles' #:action => index 
+    #article_path(@article.id)
+    	#送信したらどっかに飛んで－　redirect_to
+	end
+	def show
+		@article = Article.find(params[:id])
+	end
+	
+	private
+	def article_params
+	  params.require(:article).permit(:title, :body, :author)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/top' => 'home#top'
   root 'home#top'
-  get '/articles' => 'articles#index'
+  #get '/articles/index' => 'articles#index'
+  #get 'articles/new' => 'articles#new'
+  resources :articles
+  post '/articles' => 'articles#create'
 end
